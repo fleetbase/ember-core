d027c7681a35265161ed807b2c8e368937ef98c6
Ronald A. Richardson <me@ron.dev>
2024-07-18 00:57:49 +0800

    `UniverseService` can now handle all engine booting from either registry or indexer, also added new utility function to use native fetch to make authenticated request to the fleetbase api

---
+++ tests/unit/utils/fleetbase-api-fetch-test.js
@@ -0,0 +1,10 @@
+import fleetbaseApiFetch from 'dummy/utils/fleetbase-api-fetch';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fleetbase-api-fetch', function () {
+    // TODO: Replace this with your real tests.
+    test('it works', function (assert) {
+        let result = fleetbaseApiFetch();
+        assert.ok(result);
+    });
+});
---
+++ tests/unit/utils/load-installed-extensions-test.js
@@ -0,0 +1,10 @@
+import loadInstalledExtensions from 'dummy/utils/load-installed-extensions';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | load-installed-extensions', function () {
+    // TODO: Replace this with your real tests.
+    test('it works', function (assert) {
+        let result = loadInstalledExtensions();
+        assert.ok(result);
+    });
+});
