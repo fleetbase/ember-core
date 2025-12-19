1405f716b4cdbd793f2beb7af7b3f72577d2b8d7
Ronald A. Richardson <me@ron.dev>
2025-12-19 16:13:39 +0800

    fixed linter

--- addon/services/language.js
+++ addon/services/language.js
@@ -2,7 +2,6 @@ import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { isNone } from '@ember/utils';
 import { debug } from '@ember/debug';
 import { task } from 'ember-concurrency';
 
